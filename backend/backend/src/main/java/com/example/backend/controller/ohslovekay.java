ad5b647c5c752f60c6cd2275622e94701ea794ba
오화석 <hwasuk528@naver.com>
2024-01-02 18:46:00 +0900

    hi

---
+++ backend/backend/src/main/java/com/example/backend/controller/ohslovekay.java
@@ -0,0 +1,8 @@
+package com.example.backend.controller;
+
+public class ohslovekay {
+    public static void main(String[] args) {
+        String a = "아영언니 고마워요";
+        String b = "내가 깃천재되볼게";
+    }
+}
